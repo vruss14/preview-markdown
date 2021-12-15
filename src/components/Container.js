3ef3932192a2c55f13cb9cafab54b23b33c9eaf6
Valerie Russell <vruss14@gmail.com>
2021-12-15 13:09:52 -0500

    simplify setup & create custom stateful component that renders successfully

---
+++ src/components/Container.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+class Container extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+          name: 'Test'
+      }
+  
+    }
+    render() {
+      return (
+        <div>
+          <h1>{this.state.name}</h1>
+        </div>
+      );
+    }
+};
+
+export default Container;
